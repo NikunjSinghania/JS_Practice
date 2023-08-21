f062f0fa0b4fa39400a08efec1d582448f32171d
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-08-21 23:33:05 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //resolve('FOO')
+//         reject('FOO')
+//     }, 5000)
+//     //console.log('FOO');
+//     //res('FOO')
+//     //res()
+// }).then((r) => {
+//     console.log(r);
+// }).catch((err) => {
+//     console.log(err);
+// })
